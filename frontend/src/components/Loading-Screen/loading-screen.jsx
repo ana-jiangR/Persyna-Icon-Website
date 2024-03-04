de364cc743dcff872d1a0443f93953fe924f201b
Ana Jiang <ana.jiangvz@gmail.com>
2024-03-05 01:59:40 +0800

    update loadingscreen

--- frontend/src/components/Loading-Screen/loading-screen.jsx
+++ frontend/src/components/Loading-Screen/loading-screen.jsx
@@ -15,6 +15,15 @@ const LoadingScreen = () => {
       bodyEl.classList.add("hideX");
     }
   });
+
+  // 定义媒体查询
+  const isMobileOrTablet = window.matchMedia("(max-width: 768px)");
+
+  // 根据屏幕宽度决定是否显示加载屏幕
+  if (isMobileOrTablet.matches) {
+    return null; // 在移动设备或平板电脑上不显示加载屏幕
+  }
+
   return (
     <>
       <div className={`${appData.showLoading === true ? "showX" : "hideX"}`}>
